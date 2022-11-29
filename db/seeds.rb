c27aef6747268cfc5042c34389c3dd9f2d64b749
Manuel Kneib <manuel.kneib@gmail.com>
2022-11-29 15:57:39 +0000

    small updates on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,30 +13,33 @@ User.destroy_all
 League.destroy_all
 Match.destroy_all
 
-puts "Creating seed base"
+puts "Creating seed base, just starting..."
 names = ["Ana", "Andre_actor", "Andre_ta", "Andre_teacher", "Barbara", "Carlos", "Catarina", "Chiara", "Dal", "Danilo", "Filipa", "Gabriel_beto", "Gabriel_ta", "Gerardo", "Guilherme", "Havard", "Henrique", "Ines", "Joao", "Joaquim", "Joe", "Josh", "Julia", "Loic", "Lorenz", "Manu", "Margarida", "Miguel_bald", "Miguel_hairy", "Pedro", "Ricardo", "Rita", "Roel", "Rory", "Ryan", "Sam", "Sara_1leg", "Sara_ta", "Telo", "Vasco_young", "Vasco_old" , "Will", "Zoe"]
 i = 0
+puts "__________________"
 
-puts "Creating seed users"
+puts "Creating seed users, we're getting there..."
 43.times do
   user = User.new(
     username: names[i],
-    email: "#{names[i]}@lewapong.fun",
+    email: "#{names[i].downcase}@lewapong.fun",
     password: "123456"
   )
-  user.save
+  user.save!
   puts "#{user.username} - seeded to DB: #{user.save}"
   i += 1
 end
+puts "__________________"
 
-puts "Creating seed leagues"
+puts "Creating seed leagues, almost there..."
 15.times do
   league = League.new(
     name: "#{Faker::Superhero.power} League",
-    sport: Faker::Sports.unusual_sport
+    sport: Faker::Creature::Animal.name,
+    creator_id: # needs to be assigned
   )
-  league.save
+  league.save!
   puts "#{league.name} - seeded to DB: #{league.save}"
 end
-
-puts "Creating seed matches"
+puts "__________________"
+puts "seeding completed, let's f*cking gooooo!!!"
