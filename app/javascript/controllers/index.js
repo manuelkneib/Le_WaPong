643bddc0cc1755ee219571ed586118f768c8bc15
Andre Marques <andremarques94@gmail.com>
2022-12-01 14:44:59 +0000

    created stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import PickWinnerController from "./pick_winner_controller"
+application.register("pick-winner", PickWinnerController)
