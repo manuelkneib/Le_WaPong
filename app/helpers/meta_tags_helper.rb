5ce524599e35a5dd23134df8b92e71636d7b29a9
Manuel Kneib <manuel.kneib@gmail.com>
2022-12-03 20:31:06 +0000

    meta helpers setup

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -1,15 +1,15 @@
-=begin module MetaTagsHelper
+module MetaTagsHelper
   def meta_title
-    content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["LeWapong - digital sports tournament organization"]
+    content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["meta_title"]
   end
 
   def meta_description
-    content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["Le WaPong is a tool that helps you organize tournaments with your friends in a simple and digital way. Le WaPong was developed in a two week project at Le Wagon in Lisbon (Batch 1041)."]
+    content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["meta_description"]
   end
 
   def meta_image
-    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["logo icon.png"])
+    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["meta_image"])
     # little twist to make it work equally with an asset or a url
     meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
   end
-end =end
+end
