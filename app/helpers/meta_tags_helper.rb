a1dfa2c300e1a653ff9c7ef9cf6e2636303a8e38
Manuel Kneib <manuel.kneib@gmail.com>
2022-12-03 19:18:26 +0000

    specific meta tags

---
+++ app/helpers/meta_tags_helper.rb
@@ -0,0 +1,15 @@
+module MetaTagsHelper
+  def meta_title
+    content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["LeWapong - digital sports tournament organization"]
+  end
+
+  def meta_description
+    content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["Le WaPong is a tool that helps you organize tournaments with your friends in a simple and digital way. Le WaPong was developed in a two week project at Le Wagon in Lisbon (Batch 1041)."]
+  end
+
+  def meta_image
+    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["logo icon.png"])
+    # little twist to make it work equally with an asset or a url
+    meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
+  end
+end
