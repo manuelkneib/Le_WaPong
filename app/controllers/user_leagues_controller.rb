1aefa0e430c8bdd0a2ab96ad4ce68082803d9602
Andre Marques <andremarques94@gmail.com>
2022-12-05 12:41:26 +0000

    solved the problem of the user being redirected somewhere else than the league/:id after he/she accepts the invitation

--- app/controllers/user_leagues_controller.rb
+++ app/controllers/user_leagues_controller.rb
@@ -1,6 +1,6 @@
 class UserLeaguesController < ApplicationController
   def create
     UserLeague.create(league_id: params[:league_id], user: current_user, points: 0)
-    redirect_to leagues_path
+    redirect_to league_path(params[:league_id])
   end
 end
