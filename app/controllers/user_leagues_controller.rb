7eeb9c6afa8e25908eaa35a73cf294a7bf0652dd
Andre Marques <andremarques94@gmail.com>
2022-12-05 16:06:41 +0000

    made the redirect page after invitation work

--- app/controllers/user_leagues_controller.rb
+++ app/controllers/user_leagues_controller.rb
@@ -1,6 +1,12 @@
 class UserLeaguesController < ApplicationController
+  skip_before_action :authenticate_user!
   def create
-    UserLeague.create(league_id: params[:league_id], user: current_user, points: 0)
-    redirect_to leagues_path
+    if !current_user
+      session[:attendee] = true
+      redirect_to user_session_path
+    else
+      UserLeague.create(league_id: params[:league_id], user: current_user, points: 0)
+      redirect_to leagues_path
+    end
   end
 end
